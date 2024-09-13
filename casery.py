c3eb8e3a5974497a40cdce3e381ce67737df7152
dasdasdda342 <152520490+dasdasdda342@users.noreply.github.com>
2024-09-13 15:31:14 +0300

    Add files via upload

---
+++ casery.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["C_HRr"]
+casery = "C_HRr"
+##ايدي مطور السورس
+caserid = 6264438859
+##قناه السورس
+source = "https://t.me/COURSE_CAESAR" 
+ch = "COURSE_CAESAR" 
+##جروب السورس
+group = "https://t.me/sorescaser" 
+##لوجو السورس
+photosource = "https://telegra.ph/file/79d346793ec3e73d5e9f0.jpg"
+##توكن الصانع
+bot_token="6347821054:AAG-lCesi8s4MOO_ndhojFlvm2SgXQArJh4"
+##جلسه الاشتراك
+bot_token2="BAB86r0AtKKFlgMcYI2EbO8YAks7lIr4-WzsbGScSqnEOL5awdXYOf5XDF2CY86EmoelKIuz6Y2idQc9qIY-iITk__5QK3QpBrvWji4pHfjsJVSOWOPnwtn6v3DMTtU2uhf4vAS_8XGZ92OmqcUqNNm_tRHhI5HP8yAtsw01oNByIWZKFazdT_0P5zBoZnjJGOx8mnc5h8wRSYop-q8G-0cy8Nb9WlpKdT68xtKYtIcPIEDbAkE8VOR6z2brbiKy4SAV-KZY_RYMRZbKhwbjOv6u2En0ySV3dpiHWeFo0Y9XKoi-aZQiuq6lT6WWOQ5uZmWP-BbJe3O3ufItt9P1dmYO8YC2SwAAAAGDi9bPAA"
